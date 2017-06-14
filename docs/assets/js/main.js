6a80811986b9e6f0bebd16c7f18ea20768850cfd
Flamilani <milanidesign7@gmail.com>
2017-06-13 23:40:59 -0300

    iniciando projeto

---
+++ docs/assets/js/main.js
@@ -0,0 +1,15 @@
+ var Nav = React.createClass({
+ 	render:function(){
+ 		return (
+ 			<nav className="navbar navbar-default">
+
+ 			</nav>
+ 		);
+ 	}
+ });
+
+
+ ReactDOM.render(
+ 		<h1>React</h1>,       
+       document.getElementById('app')
+ );
